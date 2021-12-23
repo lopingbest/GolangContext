c2d8609fc2c9a4884b360b50dec652485c6e96f6
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2021-12-23 17:00:18 +0700

    feat:context_test add TestContextWithValue

--- context_test.go
+++ context_test.go
@@ -6,10 +6,32 @@ import (
 	"testing"
 )
 
+//contet bersifat imutable (tidak bisa diubah). Jika diubah maka akan membuat child baru
 func TestContext(t *testing.T) {
 	background := context.Background()
 	fmt.Println(background)
 
 	todo := context.TODO()
 	fmt.Println(todo)
+}
+
+func TestContextWithValue(t *testing.T) {
+	//parent context
+	contextA := context.Background()
+
+	//Child context A
+	contextB := context.WithValue(contextA, "b", "B")
+	contextC := context.WithValue(contextA, "c", "C")
+
+	contextD := context.WithValue(contextB, "d", "D")
+	contextE := context.WithValue(contextB, "e", "E")
+
+	contextF := context.WithValue(contextC, "f", "F")
+
+	fmt.Println(contextA)
+	fmt.Println(contextB)
+	fmt.Println(contextC)
+	fmt.Println(contextD)
+	fmt.Println(contextE)
+	fmt.Println(contextF)
 }
