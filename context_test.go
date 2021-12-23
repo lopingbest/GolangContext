1337289bde853a72a8700ada331e141ef4c20ce4
telkomdev-galihsetiadi <setiadi.galih65@gmail.com>
2021-12-23 15:38:48 +0700

    feat:context_test add TestContext

---
+++ context_test.go
@@ -0,0 +1,15 @@
+package GolangContext
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestContext(t *testing.T) {
+	background := context.Background()
+	fmt.Println(background)
+
+	todo := context.TODO()
+	fmt.Println(todo)
+}
